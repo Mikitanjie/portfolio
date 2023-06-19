b596f96eee8451e40c5f6348183e648447c21454
canozben <canozben@windowslive.com>
2023-06-19 16:46:46 +0300

    asdasd

---
+++ app/components/Header/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className= "font-bold h-full w-full text-sky-600">
+      HEADER
+    </div>
+  )
+}
+
+export default Header;
---
+++ app/components/Hero/Hero.js
@@ -0,0 +1,10 @@
+const Hero = () => {
+  return (
+    <div>
+      <h1 className="font-semibold mt-10 text-2xl text-green-500">Hero Title</h1>
+      <p className="ring-2 ring-inset ring-orange-500 font-medium">Hero Subtitle</p>
+    </div>
+  );
+}
+
+export default Hero;
