1b7f7d63cf3030bd90d1e0ef4d25010fefd23443
Mikitanjie <121625223+Mikitanjie@users.noreply.github.com>
2023-06-29 13:39:19 +0200

    Did some changes on navbar, hero and backgoundcolor

--- app/components/About/About.js
+++ app/components/About/About.js
@@ -3,7 +3,7 @@ const About = () => {
     <nopadding id="About">
     <div className="font-bold text-medium text-blue-500">
       About Me
-      <div className="font-light text-small text-black relative flex text-center">
+      <div className="font-light text-small text-white relative flex text-center">
       After many years of working in different industries and countries around Europe, dreaming about the perfect career, being a technology passionate person, I have decided to become a web developer and I recently completed Le Wagon web development bootcamp and Im ready to take on a new challenge in tech.
       After many years of working in different industries and countries around Europe, dreaming about the perfect career, being a technology passionate person, I have decided to become a web developer and I recently completed Le Wagon web development bootcamp and Im ready to take on a new challenge in tech.
       After many years of working in different industries and countries around Europe, dreaming about the perfect career, being a technology passionate person, I have decided to become a web developer and I recently completed Le Wagon web development bootcamp and Im ready to take on a new challenge in tech.
