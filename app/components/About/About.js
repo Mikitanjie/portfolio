ee63431edb50a8b0640e9453335bb2f844f0ae77
Mikitanjie <121625223+Mikitanjie@users.noreply.github.com>
2023-06-29 14:00:36 +0200

    try

--- app/components/About/About.js
+++ app/components/About/About.js
@@ -7,7 +7,7 @@ const About = () => {
       After many years of working in different industries and countries around Europe, dreaming about the perfect career, being a technology passionate person, I have decided to become a web developer and I recently completed Le Wagon web development bootcamp and Im ready to take on a new challenge in tech.
       After many years of working in different industries and countries around Europe, dreaming about the perfect career, being a technology passionate person, I have decided to become a web developer and I recently completed Le Wagon web development bootcamp and Im ready to take on a new challenge in tech.
       After many years of working in different industries and countries around Europe, dreaming about the perfect career, being a technology passionate person, I have decided to become a web developer and I recently completed Le Wagon web development bootcamp and Im ready to take on a new challenge in tech.
-
+      After many   After many 
       </div>
     </div>
     </nopadding>
