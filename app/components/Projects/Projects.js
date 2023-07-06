4a3c4ac439508beef7f89d19b3670d8b8f8f576d
Mikitanjie <121625223+Mikitanjie@users.noreply.github.com>
2023-07-06 11:19:57 +0200

    Project description and resize image

--- app/components/Projects/Projects.js
+++ app/components/Projects/Projects.js
@@ -15,23 +15,32 @@ const Projects = () => {
         <ul className="bg-emerald-700 p-4 sm:px-8 sm:pt-6 sm:pb-8 lg:p-4 xl:px-8 xl:pt-6 xl:pb-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-1 xl:grid-cols-2 gap-4 text-sm leading-6">
           <li className="project in projects">
             <div>
+              <h3 className="text-lg font-semibold">Travelpal</h3>
               <Link href="http://www.mytravelpal.xyz/" target="_blank" passHref>
-             <Image
-                style= ""
-                src="/Projectpic.png"
-                alt="Picture of the author"
-                width={200}
-                height={150}
-            />
-            </Link>
-          </div>
+                <Image
+                  style=""
+                  src="/Projectpic.png"
+                  alt="Picture of the author"
+                  width={450}
+                  height={450}
+                />
+              </Link>
+              <div>
+              <p style={{ fontSize: '17px', marginTop: '5x' }}>Web app creation:
+              </p>
+              <br />
+              <p style={{ fontSize: '15px', marginTop: '-5px' }}>Idea, pitch, implementation and shipped to production in 2 weeks.
+              </p>
+              <br />
+              <p style={{ fontSize: '15px', marginTop: '-5px' }}>Worked in a team of 4 people across different geographies.</p>
+              </div>
+            </div>
           </li>
-          <li className="flex">
-          </li>
+          <li className="flex"></li>
         </ul>
       </section>
     </div>
-      )
-    }
+  );
+}
 
 export default Projects;
