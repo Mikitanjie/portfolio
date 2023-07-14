1d35e4b4a7af8754be4e4f23d6123a6a76464aa4
Mikitanjie <121625223+Mikitanjie@users.noreply.github.com>
2023-07-14 19:10:37 +0200

    some css adjustments

--- app/_components/Projects/Projects.js
+++ app/_components/Projects/Projects.js
@@ -11,7 +11,7 @@ const Projects = () => {
         <header className="bg-emerald-600 space-y-4 p-4 sm:px-8 sm:py-6 lg:p-4 xl:px-8 xl:py-6"style={{ marginTop: '250px' }}>
           <div className="flex items-center justify-between">
 
-            <h2 className="font-semibold text-slate-900">Projects</h2>
+            <h2 className="hover:scale-110 font-semibold text-slate-900">Projects</h2>
           </div>
         </header>
         <ul className="bg-emerald-700 p-4 sm:px-8 sm:pt-6 sm:pb-8 lg:p-4 xl:px-8 xl:pt-6 xl:pb-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-1 xl:grid-cols-4 gap-4 text-sm leading-6">
@@ -20,7 +20,7 @@ const Projects = () => {
               <h3 className="text-lg font-semibold">Travelpal</h3>
               <Link href="http://www.mytravelpal.xyz/" target="_blank" passHref>
                 <Image
-                  className="object-cover"
+                  className="hover:scale-110 object-cover"
                   style=""
                   src="/Projectpic.png"
                   alt="Picture of the author"
