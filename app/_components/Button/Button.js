770325947ff74a2f6326d2dbb2585e851926910d
Mikitanjie <121625223+Mikitanjie@users.noreply.github.com>
2023-07-17 12:38:15 +0200

    Unbugged the button replacing the<link with <a tag

--- app/_components/Button/Button.js
+++ app/_components/Button/Button.js
@@ -2,16 +2,12 @@ import Link from 'next/link';
 
 const Button = () => {
   return (
-    <button type="button" className="inline-flex justify-center rounded-md bg-black
-    px-4 py-2 text-gray-500 shadow-sm ring-1 ring-inset ring-emerald-600
-    hover:bg-indigo-700	 focus:outline-offset-0" >
-      <Link href="mailto:cataniamike16@gmail.com" passHref>
-      <div className="text-green-400">
+    <button type="button" className="inline-flex justify-center rounded-lg bg-black px-4 py-2 shadow-sm ring-1 ring-inset ring-emerald-600 hover:bg-emerald-700 focus:outline-offset-0">
+      <a href="mailto:cataniamike16@gmail.com" className="text-green-400">
         Contact me
-      </div>
-      </Link>
+      </a>
     </button>
-  )
-}
+  );
+};
 
 export default Button;
