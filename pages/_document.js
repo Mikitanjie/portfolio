7d171b9314e329b90f7f59e051325e6b1f0bd0ad
Mikitanjie <121625223+Mikitanjie@users.noreply.github.com>
2024-03-11 13:31:10 +0100

    Still debugging

--- pages/_document.js
+++ pages/_document.js
@@ -1,19 +1,16 @@
-// import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Main, NextScript } from 'next/document'
 
-// class MyDocument extends Document {
-//   render() {
-//     return (
-//       <Html lang="en">
-//         <Head>
-//           <meta name="viewport" content="width=device-width, initial-scale=1" />
-//         </Head>
-//         <body>
-//           <Main />
-//           <NextScript />
-//         </body>
-//       </Html>
-//     )
-//   }
-// }
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
 
-// export default MyDocument
+export default MyDocument
