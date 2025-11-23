f89d050f3f7ed0c894a926e0ed83f270c4185719
Mikitanjie <121625223+Mikitanjie@users.noreply.github.com>
2025-11-23 23:00:11 +0100

    Fixed the hover issue for the Next.js icon, Removed the brightness effect for inverted icons, so the Next.js icon should no longer disappear on hover

--- app/_components/SkillIcon/SkillIcon.js
+++ app/_components/SkillIcon/SkillIcon.js
@@ -4,13 +4,14 @@ import { ThemeContext } from '../ThemeContext/ThemeContext';
 
 const SkillIcon = ({ href, src, alt, name, width = 80, height = 80, className = '' }) => {
   const { theme } = useContext(ThemeContext);
+  const isInverted = className.includes('icon-dark');
   
   return (
     <div className="animate-pulse hover:animate-none group relative">
       <a href={href} target="_blank" rel="noreferrer" className="block">
         <div className="transition-all duration-300 hover:scale-125 active:scale-75 hover:drop-shadow-lg">
           <Image
-            className={`transition-all duration-300 group-hover:brightness-110 ${className}`}
+            className={`transition-all duration-300 ${isInverted ? '' : 'group-hover:brightness-110'} ${className}`}
             src={src}
             alt={alt}
             width={width}
